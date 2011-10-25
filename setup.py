adb41599de766b363c7070f2fbeb1a3d108137b7
Martin Tournoij <martin@arp242.net>
2011-10-25 17:49:12 -0100

    Handle reverse DNS lookups better Don't crash if the detail window is too large to fit on the screen

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 
 setup(
 	name = 'spamdb-curses',
-	version = '1.0',
+	version = '1.1',
 	author = 'Martin Tournoij',
 	author_email = 'martin@arp242.net',
 	url = 'http://code.google.com/p/spamdb-curses/',

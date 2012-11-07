bd3d3eaafdb55724f3eb0cc6a425ee505764b025
Martin Tournoij <martin@arp242.net>
2012-11-07 20:38:58 +0100

    Fix setup.py for 1.2

--- setup.py
+++ setup.py
@@ -4,10 +4,10 @@ from distutils.core import setup
 
 setup(
 	name = 'spamdb-curses',
-	version = '1.1',
+	version = '1.2',
 	author = 'Martin Tournoij',
 	author_email = 'martin@arp242.net',
-	url = 'http://code.google.com/p/spamdb-curses/',
+	url = 'http://code.arp242.net/spamdb-curses/',
 	scripts = ['spamdb-curses'],
 	packages = ['spamdbcurses'],
 )
